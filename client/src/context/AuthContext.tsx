ce1c6c809e218d3d19efb3f49294318556fcf46b
Fargv <fernando.acedorico@abexsl.es>
2025-04-08 17:36:23 +0200

    Update AuthContext.tsx

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import * as jwtDecode from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 
 interface User {
   _id: string;
