3a78f3c0afebfde59842e1419fd60be816804b90
Fargv <fernando.acedorico@abexsl.es>
2025-04-08 13:15:32 +0200

    Create sendEmail.js
    Añadido archivo sendEmail.js temporal para evitar errores

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,6 @@
+// Función de envío de email (temporal para evitar error)
+const sendEmail = async (options) => {
+  console.log('Simulación de envío de email:', options);
+};
+
+module.exports = sendEmail;
