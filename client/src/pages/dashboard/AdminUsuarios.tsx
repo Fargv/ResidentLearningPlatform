16073841c5668dda742c08db846d3d4aec5eefb9
Fargv <fernando.acedorico@abexsl.es>
2025-04-09 16:11:03 +0200

    fix: Comentar variables y funciones no usadas

--- client/src/pages/dashboard/AdminUsuarios.tsx
+++ client/src/pages/dashboard/AdminUsuarios.tsx
@@ -32,7 +32,7 @@ import {
   //Person as PersonIcon,
   //Email as EmailIcon
 } from '@mui/icons-material';
-import { useAuth } from '../../context/AuthContext';
+//import { useAuth } from '../../context/AuthContext';
 import axios from 'axios';
 
 const AdminUsuarios: React.FC = () => {
