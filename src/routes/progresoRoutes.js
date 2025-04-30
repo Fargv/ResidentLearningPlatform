d144329b7c75e6eca96e41cdfa4beb9a3e929cc3
Fargv <fernando.acedorico@abexsl.es>
2025-04-30 16:11:19 +0200

    Feat: habilitar ruta /init/:id para inicializar progreso de residentes existentes

--- src/routes/progresoRoutes.js
+++ src/routes/progresoRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
-
 const {
+  inicializarProgresoFormativo,
   getAllProgreso,
   getProgresoResidente,
   getProgresoResidentePorFase,
