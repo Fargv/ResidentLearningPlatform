ab53874ab2a30cba53ccb626eb4eb6112170e775
Fargv <fernando.acedorico@abexsl.es>
2025-06-24 13:59:27 +0200

    fix: eliminar variable API no utilizada en AuthContext para evitar error de ESLint en build de Netlify

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import api from '../api';
 import { useNavigate } from 'react-router-dom';
 
-const API = process.env.REACT_APP_API_URL;
+//const API = process.env.REACT_APP_API_URL;
 
 // Función de decodificación manual del JWT
 function decodeJwt(token: string): any {
